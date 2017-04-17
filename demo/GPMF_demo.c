baf311970f5e5ff21b0109c7ece085d022e813f7
David <dnewman@gopro.com>
2017-04-17 11:41:11 -0700

    Sample code using the GPMF_Parser

---
+++ demo/GPMF_demo.c
@@ -0,0 +1,193 @@
+/*! @file GPMF_demo.c
+*
+*  @brief Demo to extract GPMF from an MP4
+*
+*  @version 1.0.0
+*
+*  (C) Copyright 2017 GoPro Inc (http://gopro.com/).
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+*
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+
+#include "../GPMF_parser.h"
+#include "GPMF_mp4reader.h"
+
+
+extern void PrintGPMF(GPMF_stream *ms);
+
+int main(int argc, char *argv[])
+{
+	int32_t ret = GPMF_OK;
+	GPMF_stream metadata_stream, *ms = &metadata_stream;
+	float metadatalength;
+
+	// get file return data
+	if (argc != 2)
+	{
+		printf("usage: %s <file_with_GPMF>\n", argv[0]);
+		return -1;
+	}
+
+
+	metadatalength = OpenGPMFSource(argv[1]);
+	if (metadatalength > 0.0)
+	{
+		uint32_t index, payloads = GetNumberGPMFPayloads();
+		printf("found %.2fs of metadata, from %d payloads, within %s\n", metadatalength, payloads, argv[1]);
+
+		for (index = 0; index < payloads; index++)
+		{
+			uint32_t *payload = GetGPMFPayload(index);
+			uint32_t payloadsize = GetGPMFPayloadSize(index);
+
+			ret = GPMF_Init(ms, payload, payloadsize);
+			if (ret != GPMF_OK)
+				goto cleanup;
+
+			printf("GPMF metadata for time %.3fs to %.3fs\n", metadatalength*(float)index / (float)payloads, metadatalength*(float)(index + 1) / (float)payloads);
+
+#if 1		// Output (printf) all the contained GPMF data within this payload
+			ret = GPMF_Validate(ms, GPMF_RECURVSE_LEVELS); // optional
+			if (GPMF_OK != ret)
+			{
+				printf("Invalid Structure\n");
+				goto cleanup;
+			}
+
+			GPMF_ResetState(ms);
+			do
+			{
+				PrintGPMF(ms);  // printf current GPMF KLV
+			} while (GPMF_OK == GPMF_Next(ms, GPMF_RECURVSE_LEVELS));
+			GPMF_ResetState(ms);
+			printf("\n");
+#endif
+
+
+
+#if 1		// Find all the available Streams and the data carrying FourCC, and extract pre-scaled data 
+			while (GPMF_OK == GPMF_FindNext(ms, GPMF_KEY_STREAM, GPMF_RECURVSE_LEVELS))
+			{
+				if (GPMF_OK == GPMF_SeekToSamples(ms)) //find the last FOURCC within the stream
+				{
+					uint32_t key = GPMF_Key(ms);
+					GPMF_SampleType type = GPMF_Type(ms);
+					uint32_t elements = GPMF_ElementsInStruct(ms);
+					uint32_t samples = GPMF_Repeat(ms);
+
+					printf("STRM of %c%c%c%c ", PRINTF_4CC(key));
+
+					if (type == GPMF_TYPE_COMPLEX)
+					{
+						GPMF_stream find_stream;
+						GPMF_CopyState(ms, &find_stream);
+
+						if (GPMF_OK == GPMF_FindPrev(&find_stream, GPMF_KEY_TYPE))
+						{
+							char tmp[64];
+							char *data = (char *)GPMF_RawData(&find_stream);
+							int size = GPMF_RawDataSize(&find_stream);
+
+							if (size < sizeof(tmp))
+							{
+								memcpy(tmp, data, size);
+								tmp[size] = 0;
+								printf("of type %s ", tmp);
+							}
+						}
+
+					}
+					else
+					{
+						printf("of type %c ", type);
+					}
+
+					printf("with %d sample%s ", samples, samples > 1 ? "s" : "");
+
+					if (elements > 1)
+						printf("-- %d elements per sample", elements);
+
+					printf("\n");
+				}
+			}
+			GPMF_ResetState(ms);
+			printf("\n");
+#endif 
+
+
+
+
+#if 1		// Find GPS values and return scaled floats. 
+			if (GPMF_OK == GPMF_FindNext(ms, STR2FOURCC("GPS5"), GPMF_RECURVSE_LEVELS) || //GoPro Hero5 GPS
+			    GPMF_OK == GPMF_FindNext(ms, STR2FOURCC("GPRI"), GPMF_RECURVSE_LEVELS) )   //GoPro Karma GPS
+			{
+				uint32_t samples = GPMF_Repeat(ms);
+				uint32_t elements = GPMF_ElementsInStruct(ms);
+				uint32_t buffersize = samples * elements * sizeof(float);
+				GPMF_stream find_stream;
+				float *ptr, *tmpbuffer = malloc(buffersize);
+				char units[10][6] = { "" };
+				uint32_t unit_samples = 1;
+
+				if (tmpbuffer)
+				{
+					uint32_t i, j;
+
+					//Search for any units to display
+					GPMF_CopyState(ms, &find_stream);
+					if (GPMF_OK == GPMF_FindPrev(&find_stream, GPMF_KEY_SI_UNITS) || GPMF_OK == GPMF_FindPrev(&find_stream, GPMF_KEY_UNITS))
+					{
+						char *data = (char *)GPMF_RawData(&find_stream);
+						int ssize = GPMF_StructSize(&find_stream);
+						unit_samples = GPMF_Repeat(&find_stream);
+
+						for (i = 0; i < unit_samples; i++)
+						{						
+							memcpy(units[i], data, ssize);
+							units[i][ssize] = 0;
+							data += ssize;
+						}
+					}
+
+					//GPMF_FormattedData(ms, tmpbuffer, buffersize, 0, samples); // Output data in LittleEnd, but no scale
+					GPMF_ScaledData(ms, tmpbuffer, buffersize, 0, samples, GPMF_TYPE_FLOAT);  //Output scaled data as floats
+
+					ptr = tmpbuffer;
+					for (i = 0; i < samples; i++)
+					{
+						for (j = 0; j < elements; j++)
+							printf("%.3f%s, ", *ptr++, units[j%unit_samples]);
+
+						printf("\n");
+					}
+					free(tmpbuffer);
+				}
+			}
+			GPMF_ResetState(ms);
+			printf("\n");
+#endif 
+
+		}
+
+cleanup:
+		CloseGPMFSource();
+	}
+
+	return ret;
+}
