606fc8146517b7960e0819842ea6a25d8ebcc5cf
David <dnewman@gopro.com>
2017-04-17 11:41:11 -0700

    Sample code using the GPMF_Parser

---
+++ demo/GPMF_mp4reader.c
@@ -0,0 +1,355 @@
+/*! @file GPMF_mp4reader.c
+*
+*  @brief Way Too Crude MP4 reader
+*
+*  @version 1.0.0
+*
+*  (C) Copyright 2017 GoPro Inc (http://gopro.com/).
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+*
+*/
+
+/* This is not an elegant MP4 parser, only used to help demonstrate extraction of GPMF */
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+
+#include "../GPMF_parser.h"
+
+#ifdef WIN32
+#define LONGSEEK  _fseeki64
+#else
+#define LONGSEEK  fseeko
+#endif
+
+
+uint32_t *metasizes = NULL;
+uint32_t *metaoffsets = NULL;
+uint32_t indexcount = 0;
+float videolength = 0.0;
+float metadatalength = 0.0;
+uint32_t clockdemon, clockcount;
+uint32_t trak_clockdemon, trak_clockcount;
+uint32_t meta_clockdemon, meta_clockcount;
+uint32_t basemetadataduration = 0;
+uint32_t basemetadataoffset = 0;
+FILE *fp = NULL;
+uint32_t *GPMFbuffer = NULL;
+
+
+
+typedef struct media_header
+{
+	uint8_t version_flags[4];
+	uint32_t creation_time;
+	uint32_t modification_time;
+	uint32_t time_scale;
+	uint32_t duration;
+	uint16_t language;
+	uint16_t quality;
+} media_header;
+
+
+
+
+#define MAKETAG(d,c,b,a)		(((d&0xff)<<24)|((c&0xff)<<16)|((b&0xff)<<8)|(a&0xff))
+
+
+
+uint32_t GetNumberGPMFPayloads(void)
+{
+	return indexcount;
+}
+
+
+uint32_t *GetGPMFPayload(uint32_t index)
+{
+	if (index < indexcount && fp)
+	{
+		GPMFbuffer = realloc(GPMFbuffer, metasizes[index]);
+
+		if (GPMFbuffer)
+		{
+			LONGSEEK(fp, metaoffsets[index], SEEK_SET);
+			fread(GPMFbuffer, 1, metasizes[index], fp);
+			return GPMFbuffer;
+		}
+	}
+	return NULL;
+}
+
+uint32_t GetGPMFPayloadSize(uint32_t index)
+{
+	return metasizes[index];
+}
+
+
+float OpenGPMFSource(char *filename)  //RAW or within MP4
+{
+	fp = fopen(filename, "rb");
+
+	metasizes = NULL;
+	metaoffsets = NULL;
+	indexcount = 0;
+	videolength = 0.0;
+	metadatalength = 0.0;
+	basemetadataduration = 0;
+	basemetadataoffset = 0;
+	
+	if (fp)
+	{
+		uint32_t tag, tsize, qttag, qtsize, len, skip, type = 0, num;
+		int32_t lastsize = 0, nest = 0;
+
+		len = fread(&tag, 1, 4, fp);
+		if (tag == GPMF_KEY_DEVICE) // RAW GPMF data, not in an MP4
+		{
+			int32_t num;
+			int32_t ssize;
+			int32_t bytes;
+			int filesize;
+
+			videolength += 1.0;
+			metadatalength += 1.0;
+
+			fseek(fp, 0, SEEK_END);
+			filesize = ftell(fp);
+			fseek(fp, 0, SEEK_SET);
+
+			indexcount = (int)metadatalength;
+			LONGSEEK(fp, 0, SEEK_SET); // back to start
+
+			metasizes = (uint32_t *)malloc(indexcount * 4 + 4);  memset(metasizes, 0, indexcount * 4 + 4);
+			metaoffsets = (uint32_t *)malloc(indexcount * 4 + 4);  memset(metaoffsets, 0, indexcount * 4 + 4);
+
+			metasizes[0] = (filesize)&~3;
+			metaoffsets[0] = 0;
+
+			return metadatalength;  // not an MP4, RAW GPMF which has not inherent timing, assigning a during of 1second.
+		}
+		LONGSEEK(fp, 0, SEEK_SET); // back to start
+
+		do
+		{
+			len = fread(&qtsize, 1, 4, fp);
+			len += fread(&qttag, 1, 4, fp);
+			if (len == 8)
+			{
+				qtsize = BYTESWAP32(qtsize);
+
+				if (!GPMF_VALID_FOURCC(qttag))
+				{
+					LONGSEEK(fp, lastsize - 8 - 8, SEEK_CUR);
+					nest--;
+					continue;
+				}
+				else
+					nest++;
+
+				lastsize = qtsize;
+
+				if (qttag != MAKEID('m', 'o', 'o', 'v') && //skip over all but these atoms
+					qttag != MAKEID('m', 'v', 'h', 'd') &&
+					qttag != MAKEID('t', 'r', 'a', 'k') &&
+					qttag != MAKEID('m', 'd', 'i', 'a') &&
+					qttag != MAKEID('m', 'd', 'h', 'd') &&
+					qttag != MAKEID('m', 'i', 'n', 'f') &&
+					qttag != MAKEID('g', 'm', 'i', 'n') &&
+					qttag != MAKEID('g', 'p', 'm', 'd') &&
+					qttag != MAKEID('d', 'i', 'n', 'f') &&
+					qttag != MAKEID('a', 'l', 'i', 's') &&
+					qttag != MAKEID('s', 't', 's', 'd') &&
+					qttag != MAKEID('s', 't', 's', 's') &&
+					qttag != MAKEID('s', 't', 's', 'c') &&
+					qttag != MAKEID('a', 'l', 'i', 's') &&
+					qttag != MAKEID('a', 'l', 'i', 's') &&
+					qttag != MAKEID('s', 't', 'b', 'l') &&
+					qttag != MAKEID('s', 't', 't', 's') &&
+					qttag != MAKEID('s', 't', 's', 'z') &&
+					qttag != MAKEID('s', 't', 'c', 'o') &&
+					qttag != MAKEID('h', 'd', 'l', 'r'))
+				{
+					LONGSEEK(fp, qtsize - 8, SEEK_CUR);
+					nest--;
+				}
+				else if (qttag == MAKEID('m', 'v', 'h', 'd')) //mvhd  movie header
+				{
+					len = fread(&skip, 1, 4, fp);
+					len += fread(&skip, 1, 4, fp);
+					len += fread(&skip, 1, 4, fp);
+					len += fread(&clockdemon, 1, 4, fp); clockdemon = BYTESWAP32(clockdemon);
+					len += fread(&clockcount, 1, 4, fp); clockcount = BYTESWAP32(clockcount);
+					LONGSEEK(fp, qtsize - 8 - len, SEEK_CUR); // skip over mvhd
+					nest--;
+				}
+				else if (qttag == MAKEID('m', 'd', 'h', 'd')) //mdhd  media header
+				{
+					media_header md;
+					len = fread(&md, 1, sizeof(md), fp);
+					if (len == sizeof(md))
+					{
+						md.creation_time = BYTESWAP32(md.creation_time);
+						md.modification_time = BYTESWAP32(md.modification_time);
+						md.time_scale = BYTESWAP32(md.time_scale);
+						md.duration = BYTESWAP32(md.duration);
+
+						trak_clockdemon = md.time_scale;
+						trak_clockcount = md.duration;
+
+						if (videolength == 0.0) // Get the video length from the first track
+						{
+							videolength = (float)((double)trak_clockcount / (double)trak_clockdemon);
+						}
+					}
+					LONGSEEK(fp, qtsize - 8 - len, SEEK_CUR); // skip over mvhd
+					nest--;
+				}
+				else if (qttag == MAKEID('h', 'd', 'l', 'r')) //hldr
+				{
+					len = fread(&skip, 1, 4, fp);
+					len += fread(&skip, 1, 4, fp);
+					len += fread(&type, 1, 4, fp);
+					if (len == 12)
+					{
+						if (type == MAKEID('m', 'e', 't', 'a')) // meta 
+						{
+							int32_t namelen = 0;
+							LONGSEEK(fp, 12, SEEK_CUR), len += 12; // skip to "GoPro MET"
+							fread(&namelen, 1, 1, fp), len++;
+							if (namelen >= 9)
+							{
+								char name[10];
+								fread(&name, 1, 9, fp), len += 9;
+
+								if (0 != strncmp("GoPro MET", name, 9))
+									type = 0; // not GoPro Metadata
+							}
+						}
+					}
+					LONGSEEK(fp, qtsize - 8 - len, SEEK_CUR); // skip over hldr
+					nest--;
+				}
+				else if (qttag == MAKEID('s', 't', 's', 'z')) // metadata stsz - sizes
+				{
+					if (type == MAKEID('m', 'e', 't', 'a')) // meta
+					{
+						len = fread(&skip, 1, 4, fp);
+						len += fread(&skip, 1, 4, fp);
+						len += fread(&num, 1, 4, fp);
+						num = BYTESWAP32(num);
+						if (metasizes) free(metasizes);
+						metasizes = (uint32_t *)malloc(num * 4);
+						if (metasizes)
+						{
+							len += fread(metasizes, 1, num * 4, fp);
+							do
+							{
+								num--;
+								metasizes[num] = BYTESWAP32(metasizes[num]);
+							} while (num > 0);
+						}
+						LONGSEEK(fp, qtsize - 8 - len, SEEK_CUR); // skip over stsz
+					}
+					else
+						LONGSEEK(fp, qtsize - 8, SEEK_CUR);
+
+					nest--;
+				}
+				else if (qttag == MAKEID('s', 't', 'c', 'o')) // metadata stco - offsets
+				{
+					if (type == MAKEID('m', 'e', 't', 'a')) // meta
+					{
+						len = fread(&skip, 1, 4, fp);
+						len += fread(&num, 1, 4, fp);
+						num = BYTESWAP32(num);
+						indexcount = num;
+						if (metaoffsets) free(metaoffsets);
+						metaoffsets = (uint32_t *)malloc(num * 4);
+						if (metaoffsets)
+						{
+							len += fread(metaoffsets, 1, num * 4, fp);
+							do
+							{
+								num--;
+								metaoffsets[num] = BYTESWAP32(metaoffsets[num]);
+							} while (num > 0);
+						}
+						LONGSEEK(fp, qtsize - 8 - len, SEEK_CUR); // skip over stco
+					}
+					else
+						LONGSEEK(fp, qtsize - 8, SEEK_CUR);
+
+					nest--;
+				}
+				else if (qttag == MAKEID('s', 't', 't', 's')) // time to samples
+				{
+					if (type == MAKEID('m', 'e', 't', 'a')) // meta 
+					{
+						uint32_t totaldur = 0;
+						int32_t entries = 0;
+						len = fread(&skip, 1, 4, fp);
+						len += fread(&num, 1, 4, fp);
+						num = BYTESWAP32(num);
+						entries = num;
+
+						meta_clockdemon = trak_clockdemon;
+						meta_clockcount = trak_clockcount;
+
+						while (entries > 0)
+						{
+							int32_t samplecount;
+							int32_t duration;
+							len += fread(&samplecount, 1, 4, fp);
+							samplecount = BYTESWAP32(samplecount);
+							len += fread(&duration, 1, 4, fp);
+							duration = BYTESWAP32(duration);
+
+							if (samplecount > 1)
+							{
+								basemetadataoffset = totaldur;
+								basemetadataduration = duration;
+							}
+							entries--;
+
+							totaldur += duration;
+							metadatalength += (float)((double)samplecount * (double)duration / (double)meta_clockdemon);
+						}
+						LONGSEEK(fp, qtsize - 8 - len, SEEK_CUR); // skip over stco
+					}
+					else
+						LONGSEEK(fp, qtsize - 8, SEEK_CUR);
+
+					nest--;
+				}
+			}
+			else
+				break;
+
+		} while (len > 0);
+	}
+
+	return metadatalength;
+}
+
+
+void CloseGPMFSource(void)
+{
+	if (fp) fclose(fp), fp = NULL;
+	if (metasizes) free(metasizes), metasizes = 0;
+	if (metaoffsets) free(metaoffsets), metaoffsets = 0;
+	if (GPMFbuffer) free(GPMFbuffer), GPMFbuffer = NULL;
+}
---
+++ demo/GPMF_print.c
@@ -0,0 +1,478 @@
+/*
+*  Unpublished Copyright (c) 2013-17 GoPro, Inc., All Rights Reserved.
+*
+*  GoPro, Inc. ("GoPro") CONFIDENTIAL
+*
+*  NOTICE: All information contained herein is, and remains the property of
+*  GoPro. The intellectual and technical concepts contained herein are
+*  proprietary to GoPro and may be covered by U.S. and Foreign Patents, patents
+*  in process, and are protected by trade secret or copyright law.
+*  Dissemination of this information or reproduction of this material is
+*  strictly forbidden unless prior written permission is obtained from GoPro.
+*  Access to the source code contained herein is hereby forbidden to anyone
+*  except current GoPro employees, managers or contractors who have executed
+*  Confidentiality and Non-disclosure agreements explicitly covering such
+*  access.
+*
+*  The copyright notice above does not evidence any actual or intended
+*  publication or disclosure of this source code, which includes information
+*  that is confidential and/or proprietary, and is a trade secret, of GoPro.
+*  ANY REPRODUCTION, MODIFICATION, DISTRIBUTION, PUBLIC PERFORMANCE, OR PUBLIC
+*  DISPLAY OF OR THROUGH USE OF THIS SOURCE CODE WITHOUT THE EXPRESS WRITTEN
+*  CONSENT OF GOPRO IS STRICTLY PROHIBITED, AND IN VIOLATION OF APPLICABLE LAWS
+*  AND INTERNATIONAL TREATIES.  THE RECEIPT OR POSSESSION OF THIS SOURCE CODE
+*  AND/OR RELATED INFORMATION DOES NOT CONVEY OR IMPLY ANY RIGHTS TO REPRODUCE,
+*  DISCLOSE OR DISTRIBUTE ITS CONTENTS, OR TO MANUFACTURE, USE, OR SELL ANYTHING
+*  THAT IT MAY DESCRIBE, IN WHOLE OR IN PART.
+*
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+
+#include "../GPMF_parser.h"
+
+
+#if _WINDOWS
+#define DBG_MSG printf
+#else
+#define DBG_MSG(...)
+#endif
+
+
+#define VERBOSE_OUTPUT		0
+
+#if VERBOSE_OUTPUT
+#define LIMITOUTPUT		arraysize = structsize;
+#else
+#define LIMITOUTPUT		if (arraysize > 1 && repeat > 3) repeat = 3, dots = 1; else if (repeat > 6) repeat = 6, dots = 1;
+#endif
+
+void printfData(uint32_t type, uint32_t structsize, uint32_t repeat, void *data)
+{
+	int dots = 0;
+
+	switch (type)
+	{
+	case GPMF_TYPE_STRING_ASCII:
+		{
+			char t[256];
+			int size = structsize*repeat;
+			uint32_t arraysize = structsize;
+			LIMITOUTPUT;
+
+			if (size > 255)
+			{
+				size = 255;
+			}
+			memcpy(t, (char *)data, size);
+			t[size] = 0;
+
+			if (arraysize == 1 || repeat == 1)
+			{
+				DBG_MSG("\"%s\"", t);
+				dots = 0;
+			}
+			else
+			{
+				uint32_t i,j,pos=0;
+				for (i = 0; i < repeat; i++)
+				{
+					DBG_MSG("\"");
+					for (j = 0; j < arraysize; j++)
+					{
+						if (t[pos] != '\0' && t[pos] != ' ')
+							DBG_MSG("%c", t[pos]);
+						pos++;
+					}
+					DBG_MSG("\", ");
+				}
+			}
+		}
+		break;
+	case GPMF_TYPE_SIGNED_BYTE:
+		{
+			int8_t *b = (int8_t *)data;
+			uint32_t arraysize = structsize;
+			LIMITOUTPUT;
+
+			while (repeat--)
+			{
+				arraysize = structsize;
+
+				while (arraysize--)
+				{
+					DBG_MSG("%d,", (int8_t)*b);
+					b++;
+				}
+				if (repeat) DBG_MSG(" ");
+			}
+		}
+		break;
+	case GPMF_TYPE_UNSIGNED_BYTE:
+		{
+			uint8_t *b = (uint8_t *)data;
+			uint32_t arraysize = structsize;
+			LIMITOUTPUT;
+
+			while (repeat--)
+			{
+				arraysize = structsize;
+
+				while (arraysize--)
+				{
+					DBG_MSG("%d,", *b);
+					b++;
+				}
+				if (repeat) DBG_MSG(" ");
+			}
+		}
+		break;
+	case GPMF_TYPE_DOUBLE:	
+		{
+			uint64_t Swap, *L = (uint64_t *)data;
+			double *d;
+			uint32_t arraysize = structsize / sizeof(uint64_t);
+			LIMITOUTPUT;
+
+			while (repeat--)
+			{
+				arraysize = structsize / sizeof(uint64_t);
+
+				while (arraysize--)
+				{
+					Swap = BYTESWAP64(*L);
+					d = (double *)&Swap;
+					DBG_MSG("%.3f,", *d);
+					L++;
+				}
+				if (repeat) DBG_MSG(" ");
+			}
+		}
+		break;
+	case GPMF_TYPE_FLOAT:
+		{
+			uint32_t Swap, *L = (uint32_t *)data;
+			float *f;
+			uint32_t arraysize = structsize / sizeof(uint32_t);
+			LIMITOUTPUT;
+
+			while (repeat--)
+			{
+				arraysize = structsize / sizeof(uint32_t);
+
+				while (arraysize--)
+				{
+					Swap = BYTESWAP32(*L);
+					f = (float *)&Swap;
+					DBG_MSG("%.3f,", *f);
+					L++;
+				}
+				if (repeat) DBG_MSG(" ");
+			}
+		}
+	break;
+	case GPMF_TYPE_FOURCC:
+		{
+			uint32_t *L = (uint32_t *)data;
+			uint32_t arraysize = structsize / sizeof(uint32_t);
+			LIMITOUTPUT;
+			while (repeat--)
+			{
+				arraysize = structsize / sizeof(uint32_t);
+
+				while (arraysize--)
+				{
+					DBG_MSG("%c%c%c%c,", PRINTF_4CC(*L));
+					L++;
+				}
+				if (repeat) DBG_MSG(" ");
+			}
+		}
+		break;
+	case GPMF_TYPE_GUID: // display GUID in this formatting ABCDEF01-02030405-06070809-10111213
+		{
+			uint8_t *B = (uint8_t *)data;
+			uint32_t arraysize = structsize;
+			LIMITOUTPUT;
+			while (repeat--)
+			{
+				arraysize = structsize;
+
+				while (arraysize--)
+				{
+					DBG_MSG("%02X", *B);
+					B++;
+				}
+				if (repeat) DBG_MSG(" ");
+			}
+		}
+		break;
+	case GPMF_TYPE_SIGNED_SHORT:
+		{
+			int16_t *s = (int16_t *)data;
+			uint32_t arraysize = structsize / sizeof(int16_t);
+			LIMITOUTPUT;
+
+			while (repeat--)
+			{
+				arraysize = structsize / sizeof(int16_t);
+
+				while (arraysize--)
+				{
+					DBG_MSG("%d,", (int16_t)BYTESWAP16(*s));
+					s++;
+				}
+				if (repeat) DBG_MSG(" ");
+			}
+		}
+		break;
+	case GPMF_TYPE_UNSIGNED_SHORT:
+		{
+			uint16_t *S = (uint16_t *)data;
+			uint32_t arraysize = structsize / sizeof(uint16_t);
+			LIMITOUTPUT;
+
+			while (repeat--)
+			{
+				arraysize = structsize / sizeof(uint16_t);
+
+				while (arraysize--)
+				{
+					DBG_MSG("%d,", BYTESWAP16(*S));
+					S++;
+				}
+				if (repeat) DBG_MSG(" ");
+			}
+		}
+		break;
+	case GPMF_TYPE_SIGNED_LONG:
+		{
+			int32_t *l = (int32_t *)data;
+			uint32_t arraysize = structsize / sizeof(uint32_t);
+			LIMITOUTPUT;
+
+			while (repeat--)
+			{
+				arraysize = structsize / sizeof(uint32_t);
+
+				while (arraysize--)
+				{
+					DBG_MSG("%d,", (int32_t)BYTESWAP32(*l));
+					l++;
+				}
+				if (repeat) DBG_MSG(" ");
+			}
+		}
+		break;
+	case GPMF_TYPE_UNSIGNED_LONG:
+		{
+			uint32_t *L = (uint32_t *)data;
+			uint32_t arraysize = structsize / sizeof(uint32_t);
+			LIMITOUTPUT;
+			while (repeat--)
+			{
+				arraysize = structsize / sizeof(uint32_t);
+
+				while (arraysize--)
+				{
+					DBG_MSG("%d,", BYTESWAP32(*L));
+					L++;
+				}
+				if(repeat) DBG_MSG(" ");
+			}
+		}
+		break;
+	case GPMF_TYPE_Q15_16_FIXED_POINT:		
+		{
+			int32_t *q = (int32_t *)data;
+			uint32_t arraysize = structsize / sizeof(int32_t);
+			LIMITOUTPUT;
+
+			while (repeat--)
+			{
+				arraysize = structsize / sizeof(int32_t);
+
+				while (arraysize--)
+				{
+					double dq = BYTESWAP32(*q);
+					dq /= (double)65536.0;
+					DBG_MSG("%.3f,", dq);
+					q++;
+				}
+				if (repeat) DBG_MSG(" ");
+			}
+		}
+		break;
+	case GPMF_TYPE_Q31_32_FIXED_POINT:
+		{
+			int64_t *Q = (int64_t *)data;
+			uint32_t arraysize = structsize / sizeof(int64_t);
+			LIMITOUTPUT;
+
+			while (repeat--)
+			{
+				arraysize = structsize / sizeof(int64_t);
+
+				while (arraysize--)
+				{
+					uint64_t Q64 = (uint64_t)BYTESWAP64(*Q);
+					double dq = (double)(Q64 >> (uint64_t)32);
+					dq += (double)(Q64 & (uint64_t)0xffffffff) / (double)0x100000000;
+					DBG_MSG("%.3f,", dq);
+					Q++;
+				}
+				if (repeat) DBG_MSG(" ");
+			}
+		}
+		break;
+	case GPMF_TYPE_UTC_DATE_TIME:
+		{
+			char *U = (char *)data;
+			uint32_t arraysize = structsize / 16;
+			LIMITOUTPUT;
+			while (repeat--)
+			{
+				arraysize = structsize / 16;
+				char t[17];
+				t[16] = 0;
+
+				while (arraysize--)
+				{
+					strncpy(t, U, 16);
+					DBG_MSG("\"%s\",", t);
+					U += 16;
+				}
+				if (repeat) DBG_MSG(" ");
+			}
+		}
+		break;
+	case GPMF_TYPE_SIGNED_64BIT_INT:
+		{
+			int64_t *J = (int64_t *)data;
+			uint32_t arraysize = structsize / sizeof(int64_t);
+			LIMITOUTPUT;
+			while (repeat--)
+			{
+				arraysize = structsize / sizeof(int64_t);
+
+				while (arraysize--)
+				{
+					DBG_MSG("%lld,", BYTESWAP64(*J));
+					J++;
+				}
+				if (repeat) DBG_MSG(" ");
+			}
+		}
+		break;
+	case GPMF_TYPE_UNSIGNED_64BIT_INT:
+		{
+			uint64_t *J = (uint64_t *)data;
+			uint32_t arraysize = structsize / sizeof(uint64_t);
+			LIMITOUTPUT;
+			while (repeat--)
+			{
+				arraysize = structsize / sizeof(uint64_t);
+
+				while (arraysize--)
+				{
+					DBG_MSG("%llu,", BYTESWAP64(*J));
+					J++;
+				}
+				if (repeat) DBG_MSG(" ");
+			}
+		}
+		break;
+	default:
+		break;
+	}
+
+	if (dots) // more data was not output
+		DBG_MSG("...");
+}
+
+
+void PrintGPMF(GPMF_stream *ms)
+{
+	if (ms)
+	{
+		uint32_t key = GPMF_Key(ms);
+		uint32_t type = GPMF_Type(ms);
+		uint32_t structsize = GPMF_StructSize(ms);
+		uint32_t repeat = GPMF_Repeat(ms);
+		uint32_t size = GPMF_RawDataSize(ms);
+		uint32_t level = GPMF_NestLevel(ms);
+		void *data = GPMF_RawData(ms);
+
+		if (key != GPMF_KEY_DEVICE) level++;
+
+		while (level > 0 && level < 10) DBG_MSG("  "), level--;
+		if (type == 0)
+			DBG_MSG("%c%c%c%c nest size %d ", (key >> 0) & 0xff, (key >> 8) & 0xff, (key >> 16) & 0xff, (key >> 24) & 0xff, size);
+		else if (structsize == 1 || (repeat == 1 && type != '?'))
+			DBG_MSG("%c%c%c%c type '%c' size %d ", (key >> 0) & 0xff, (key >> 8) & 0xff, (key >> 16) & 0xff, (key >> 24) & 0xff, type == 0 ? '0' : type, size);
+		else
+			DBG_MSG("%c%c%c%c type '%c' struct %d repeat %d ", (key >> 0) & 0xff, (key >> 8) & 0xff, (key >> 16) & 0xff, (key >> 24) & 0xff, type == 0 ? '0' : type, structsize, repeat);
+
+		if (type && repeat > 0)
+			DBG_MSG("data: ");
+
+		if (type == GPMF_TYPE_COMPLEX)
+		{
+			GPMF_stream find_stream;
+			GPMF_CopyState(ms, &find_stream);
+			if (GPMF_OK == GPMF_FindPrev(&find_stream, GPMF_KEY_TYPE))
+			{
+				char *srctype = GPMF_RawData(&find_stream);
+				uint32_t typelen = GPMF_RawDataSize(&find_stream);
+				char typearray[64];
+				int dstsize = 64;
+				int struct_size_of_type;
+
+				GPMF_ExpandComplexTYPE(srctype, typelen, typearray, dstsize);
+				struct_size_of_type = GPMF_SizeOfComplexTYPE(typearray);
+				if (struct_size_of_type != (int32_t)structsize)
+				{
+					DBG_MSG("error: found structure of %d bytes reported as %d bytes", struct_size_of_type, structsize);
+				}
+				else
+				{
+					uint8_t *bdata = (uint8_t *)data;
+					int i;
+#if VERBOSE_OUTPUT
+					uint32_t j;
+					for (j = 0; j < repeat; j++)
+					{
+						if( repeat > 1) DBG_MSG("\n");
+#endif
+						dstsize = strlen(typearray);
+						for (i = 0; i < dstsize; i++)
+						{
+							int elementsize = GPMF_SizeofType(typearray[i]);
+							printfData(typearray[i], elementsize, 1, bdata);
+							bdata += elementsize;
+						}
+#if VERBOSE_OUTPUT
+					}
+#else
+					if (repeat > 1)
+						DBG_MSG("...");
+#endif
+				}
+			}
+			else
+			{
+				DBG_MSG("unknown formatting");
+			}
+
+		}
+		else
+		{
+			printfData(type, structsize, repeat, data);
+		}
+
+		DBG_MSG("\n");
+	}
+}
