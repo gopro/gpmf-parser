d53dfb8052838b724e756802fa806333d56f3a0f
Teeto Cheema <tcheema@gopro.com>
2019-08-15 11:53:15 -0700

    Fixed a memory leak in CloseSource

--- demo/GPMF_mp4reader.c
+++ demo/GPMF_mp4reader.c
@@ -821,11 +821,31 @@ uint32_t GetVideoFrameRateAndCount(size_t handle, uint32_t *numer, uint32_t *dem
 void CloseSource(size_t handle)
 {
 	mp4object *mp4 = (mp4object *)handle;
-	if (mp4 == NULL) return;
+	if (mp4 == NULL) 
+	{
+		return;
+	}
 
-	if (mp4->mediafp) fclose(mp4->mediafp), mp4->mediafp = NULL;
-	if (mp4->metasizes) free(mp4->metasizes), mp4->metasizes = 0;
-	if (mp4->metaoffsets) free(mp4->metaoffsets), mp4->metaoffsets = 0;
+	if (mp4->mediafp)
+	{
+		fclose(mp4->mediafp);
+		mp4->mediafp = NULL;
+	}
+	if (mp4->metasizes)
+	{
+		free(mp4->metasizes);
+		mp4->metasizes = 0;
+	}
+	if (mp4->metaoffsets)
+	{
+		free(mp4->metaoffsets);
+		mp4->metaoffsets = 0;
+	}
+	if (mp4->metastsc)
+	{
+		free(mp4->metastsc);
+		mp4->metastsc = 0;
+	}
 
 	free(mp4);
 }
